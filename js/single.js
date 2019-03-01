7b51d42bb7b0edd48722f280bdb6302b1b688e14
ibrahim <josephibrahi@gmail.com>
2019-03-01 07:53:12 +0100

    Backend (fix): added fix to single view to display properly

--- js/single.js
+++ js/single.js
@@ -1,45 +1,51 @@
 $(document).ready(function(){
 
-    // get single user infor from localforage
-    var single_data = localStorage.getItem("singleView");
-    //  console.log(single_data);
-    // single_data =(single_data+`"}`);
-    // single_data =JSON.parse(single_data);
-    return console.log(single_data);
-    // view for Aiki
+    //get user id and aiki id
+    var n = new URLSearchParams(window.location.search);
+    n.has('user_id');
+    var userId = n.get('user_id');
+    var aiki_id = n.get('aiki_id');
 
-    //create html element and append it
-    var aiki_card = `
-    <p class="h2">${single_data.desciption}</p>
-    <small class="row ml-0">rating</small>
-    <hr />
-    <div class="row pr-5">
-        <span class="col">${single_data.amount}</span>
-        <a href="#" class="col btn btn-primary ">
-            Continue
-        </a>
-    </div>
-    `;
+    $.ajax({
+        url: `http://localhost:3000/aiki/${aiki_id}`,
+        method: "GET",
+    }).done(function(resp){
+        //create html element and append it
+        var aiki_card = `
+        <p class="h2">${resp.description}</p>
+        <small class="row ml-0">rating</small>
+        <hr />
+        <div class="row pr-5">
+            <span class="col">${resp.amount}</span>
+            <a href="tel:+2348036798575" class="col btn btn-primary ">
+                Call Me
+            </a>
+        </div>
+        `;
+    
+        $("#singleView").append(aiki_card);
+    });
 
-    $("#singleView").append(aiki_card);
+    $.ajax({
+        url: `http://localhost:3000/users/${userId}`,
+        method: "GET",
+    }).done(function(resp){
+        console.log(resp);
+        // user information cards
+        var user_card = `
+        <p class="h2">${resp.fname + " " + resp.lname}</p>
+        <small class="row ml-0">${resp.email}</small>
+        <hr />
+        <p class="h4">${resp.country}</p>
+        <p class="h6">${resp.city}</p>
+        <hr />
+        <div class="row pr-5">
+            <span class="col">${resp.about}</span>
+        </div>
+        `;
+    
+        $("#user-infor-card").append(user_card);
+    });
 
-    // user information cards
-    var user_card = `
-    <p class="h2">${single_data.user.fname + " "+ single_data.user.lname}</p>
-    <small class="row ml-0">${single_data.user.email}</small>
-    <hr />
-    <p class="h5">${single_data.user.country}</p>
-    <ul class="">
-        <li>
-            <small>${single_data.user.city}</small>
-        </li>
-    </ul>
-    <hr />
-    <div class="row pr-5">
-        <span class="col">${single_data.user.address}</span>
-    </div>
-    `;
-
-    $("#user-infor-card").append(user_card);
 
 });
