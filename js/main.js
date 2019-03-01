b71d0b3829055627077eff58ce443bdc78b4ea4c
ibrahim <josephibrahi@gmail.com>
2019-03-01 01:53:23 +0100

    make it better

--- js/main.js
+++ js/main.js
@@ -9,10 +9,7 @@
             alert("Sign in first");
         }
     });
-    // get user from localstorage
-    // var user = (localStorage.getItem("user"));
-    // alert(user);
-    // console.log(user);
+
     // enable input field for user profile
     $("#enableInputField").on("click", function () {
         $(".form-control").prop("disabled", false);
@@ -187,17 +184,42 @@
         });
         $("#jobberMan").append(jobber_man);
 
-        $(document).on("click", "#singleView", function(e){
+        $(document).on("click", "#singleView", function (e) {
             e.preventDefault();
             var data = $(this).attr("href");
             // return console.log()
             // save on localstorage
-            localStorage.setItem("singleView", data +`"}}`);
+            localStorage.setItem("singleView", data + `"}}`);
             localStorage.setItem("u_desc", data);
             window.location.replace("single.html");
         })
 
     });
+
+    /// serch functionalty 
+    $("#searchJobberman").keyup(function (e) {
+        $.ajaxSetup({ cache: false });
+        $('#search').keyup(function () {
+            $('#result').html('');
+            $('#state').val('');
+            var searchField = $('#search').val();
+            var expression = new RegExp(searchField, "i");
+            $.getJSON('data.json', function (data) {
+                $.each(data, function (key, value) {
+                    if (value.name.search(expression) != -1 || value.location.search(expression) != -1) {
+                        $('#result').append('<li class="list-group-item link-class"><img src="' + value.image + '" height="40" width="40" class="img-thumbnail" /> ' + value.name + ' | <span class="text-muted">' + value.location + '</span></li>');
+                    }
+                });
+            });
+        });
+
+        $('#result').on('click', 'li', function () {
+            var click_text = $(this).text().split('|');
+            $('#search').val($.trim(click_text[0]));
+            $("#result").html('');
+        });
+    });
+});
 
 
 
