916bb2812e1f374e0872dc7026308745c12a4da4
ibrahim <josephibrahi@gmail.com>
2019-03-12 17:55:26 +0100

    heroku setup commit: note all changes are not properly commited to this repo

---
+++ public/js/dashboard.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    // //check to see if it is loading 
+    // alert("am working");
+
+
+    // capture the link and load the appropriate content for it
+    // $(".verticalMenuLeft").on("click", function (e) {
+    //     e.preventDefault();
+
+    //     // targetted element text value
+    //     var menu_link = e.target.innerText;
+    //     // console.log(menu_link);
+    //     switch (menu_link) {
+    //         case "Dashboard":
+    //         $("#loadedContent").load("user_profile.html");
+    //             break;
+    //         case "User profile":
+    //             alert("User profile clicked");
+    //             break;
+    //         case "Location":
+    //             alert("Location clicked");
+    //             break;
+    //         case "My Aiki":
+    //             alert("My Aiki clicked");
+    //             break;
+    //         case "Logout":
+    //             alert("Logout clicked");
+    //             break;
+    //         case "Getting started":
+    //             alert("Getting started clicked");
+    //             break;
+    //         default: ;
+    //     }
+    // });
+});
